e3b400231b51653e4d00693d8337529cc0397b8a
adriannapinzariu <121136061+adriannapinzariu@users.noreply.github.com>
2024-04-15 14:52:35 -0500

    to List

--- src/main/scala/worksheet_5/MyList.scala
+++ src/main/scala/worksheet_5/MyList.scala
@@ -1,4 +1,6 @@
 trait MyList[+X]
+//case object MyNil extends MyList[Nothing]
+//case class MyCons[+X] ()
 
 def length [X] (xs:MyList[X]) : Int = {
     xs match { // pattern match
@@ -16,7 +18,25 @@ def length [X] (xs:MyList[X]) : Int = {
 }
 
 def toList [X] (xs:MyList[X]) : Int = {
-    
+    xs match {
+        case MyNil => 0
+        case MyCons(y, ys) => y::toList(ys)
+                            // head::toList(tail)
+        // (y, ys) because you need to extract both the head and tail, you don't need to ignore the head
+        // see here you need the actual value, there you don't you just need the count
+        // y::toList(ys)
+            // prepend y to ys
+
+
+        // ex. val myList = MyCons(1, MyCons(2, MyCons(3, MyNil)))
+            // y = 1 and ys = MyCons(2, MyCons(3, MyNil))
+            // once recursion is over you return the prepend
+
+            // Return from Fourth Call: Returns Nil.
+            // Return from Third Call: Returns 3 :: Nil which results in List(3).
+            // Return from Second Call: Returns 2 :: List(3) which results in List(2, 3).
+            //Return from First Call: Returns 1 :: List(2, 3) which results in List(1, 2, 3).
+    }
 }
 
 def fromList [X] (xs:List[X]) : MyList[X] = {
