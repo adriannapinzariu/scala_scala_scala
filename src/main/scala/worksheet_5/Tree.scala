8b2788259501794a70209e2c04fe720040856b7b
adriannapinzariu <121136061+adriannapinzariu@users.noreply.github.com>
2024-04-17 18:31:23 -0500

    insert

--- src/main/scala/worksheet_5/Tree.scala
+++ src/main/scala/worksheet_5/Tree.scala
@@ -12,6 +12,7 @@ val tree1:Tree[Int] = Leaf // val is immutable & var is mutable
 val tree2:Tree[Int] = Node (Leaf, 5, Leaf)
 val tree3:Tree[Int] = Node (Node (Leaf, 2, Leaf), 3, Node(Leaf, 4, Leaf))
 
+/*
 def size [X] (t:Tree[X]): Int = {
     t match { // this is t because you need to match the variable name in the parameter
         case Leaf => 0;
@@ -19,13 +20,24 @@ def size [X] (t:Tree[X]): Int = {
         // you could choose to ignore the middle or not, your choice really
     }
 }
-
-
-
+*/
 
 def size [X] (t: Tree[X]): Int = {
     t match {
         case Leaf => 0;
         case Node(left, _, right) => 1 + size(left) + size(right);
+    }
+}
+
+def insert [X] (x:X, t:Tree[X], lt:(X,X)=>Boolean) : Tree[X] = {
+    t match {
+        case Leaf => Node(Leaf, x, Leaf);
+            // encountering an empty leaf means you have reached a point of insertion
+            // you put x because it's the variable you want to insert
+        case Node (left, mid, right) if (lt (x, mid)) => Node(insert(x, left, lt), c, t2);
+            // lt is less than
+        case Node (left, mid, right) if (lt (mid, x)) => Node(insert(x, right, lt));
+        case Node (left, mid, right) if (x == mid) => Node(left, mid, right);
+            // NO INSERTION BECAUSE DUPLICATE
     }
 }
